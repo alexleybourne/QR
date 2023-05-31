c5b268b7dfdfaf72a24cf8dd58d81c85d1eecfc2
alexleybourne <alexleybourne1@gmail.com>
2023-05-31 22:08:48 +1000

    It works! 👌🏻

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
+'use client';
+
+import React, { useState } from 'react';
+
 import Image from 'next/image';
+import { QRCode } from 'react-qrcode-logo';
 
 export default function Home() {
+  const [input, setInput] = useState('');
+  const twitterLink = 'https://twitter.com/AlexLeybourne';
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
-        <p className='fixed left-0 top-0 flex w-full justify-center border-b border-violet-300 bg-gradient-to-b from-violet-200 pb-6 pt-8 backdrop-blur-2xl dark:border-violet-800 dark:bg-violet-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-violet-200 lg:p-4 lg:dark:bg-violet-800/30'>
-          &nbsp;
-          <code className='font-mono font-bold'>
-            Paste a link to get started
-          </code>
-        </p>
+        <p className='w-1/4'>Create a QR Code quickly and easily for free 🤩</p>
         <div className='fixed bottom-0 left-0 flex h-48 w-full items-end justify-center flex-col bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
           <Image
             src='/qr-code-logo.svg'
@@ -20,7 +27,7 @@ export default function Home() {
           />
           <a
             className='pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0 opacity-60 transition duration-300 ease-in-out hover:opacity-100 mt-2'
-            href='https://twitter.com/AlexLeybourne'
+            href={twitterLink}
             target='_blank'
             rel='noopener noreferrer'
           >
@@ -29,77 +36,33 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/4 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[400px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-violet-700 before:dark:opacity-10 after:dark:from-violet-900 after:dark:via-[#6F3EF0] after:dark:opacity-40 before:lg:h-[360px]"></div>
-
-      <div className='mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left'>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-violet-300 hover:bg-violet-100 hover:dark:border-violet-700 hover:dark:bg-violet-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-violet-300 hover:bg-violet-100 hover:dark:border-violet-700 hover:dark:bg-violet-800 hover:dark:bg-opacity-30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-violet-300 hover:bg-violet-100 hover:dark:border-violet-700 hover:dark:bg-violet-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-violet-300 hover:bg-violet-100 hover:dark:border-violet-700 hover:dark:bg-violet-800/30'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="relative h-[50vh] w-full flex justify-center place-items-center before:absolute before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[200px] after:w-[400px] after:translate-x-2 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-violet-700 before:dark:opacity-10 after:dark:from-violet-500 after:dark:via-[#6F3EF0] after:dark:opacity-40 before:lg:h-[360px]">
+        <div className='flex flex-col items-center align-center justify-center gap-8'>
+          <QRCode value={input.length > 1 ? input : twitterLink} size={200} />
+        </div>
       </div>
+
+      <p className='fixed left-0 top-0 flex w-[50vw] justify-center border-b border-violet-300 bg-gradient-to-b from-violet-200 pb-2 pt-2 backdrop-blur-2xl dark:border-violet-800 dark:bg-violet-800/30 dark:from-inherit lg:static lg:rounded-xl lg:border lg:bg-violet-200 p-4 lg:dark:bg-violet-800/30'>
+        &nbsp;
+        <input
+          value={input}
+          onChange={handleInputChange}
+          placeholder='Paste a link to get started'
+          className='fixed left-0 top-0 flex w-full justify-center lg:static text-center text-white text-md bg-transparent border-none outline-none font-mono font-bold'
+        />
+      </p>
+
+      <footer className='flex mt-20'>
+        <p className='opacity-40'>Created</p>
+        <a
+          className='mx-2 opacity-60 transition duration-300 hover:opacity-100'
+          href={twitterLink}
+          target='_blank'
+        >
+          Alex Leybourne
+        </a>{' '}
+        <p className='opacity-40'>2022.</p>
+      </footer>
     </main>
   );
 }
